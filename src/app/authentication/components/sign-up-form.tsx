b55df2bd9c3fdda168041d54745259aed254b029
HenriquePereiraa <henrique802011@gmail.com>
2025-08-09 11:47:47 -0300

    feat: add sign up

--- src/app/authentication/components/sign-up-form.tsx
+++ src/app/authentication/components/sign-up-form.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 import z from "zod";
 
 import { Button } from "@/components/ui/button";
@@ -22,6 +24,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { authClient } from "@/lib/auth-client";
 
 const formSchema = z
   .object({
@@ -43,6 +46,7 @@ const formSchema = z
 type FormValues = z.infer<typeof formSchema>;
 
 const SignUpForm = () => {
+  const router = useRouter();
   const form = useForm<FormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -53,7 +57,27 @@ const SignUpForm = () => {
     },
   });
 
-  function onSubmit(value: FormValues) {}
+  async function onSubmit(value: FormValues) {
+    await authClient.signUp.email({
+      name: value.name,
+      email: value.email,
+      password: value.password,
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/");
+        },
+        onError: (error) => {
+          if (error.error.code === "USER_ALREADY_EXISTS") {
+            toast.error("E-mail ou senha inválidos.");
+            form.setError("email", {
+              message: "Email já cadastrado.",
+            });
+          }
+          toast.error(error.error.message);
+        },
+      },
+    });
+  }
 
   return (
     <>
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -22,7 +22,7 @@ userAgent: text('user_agent'),
 userId: text('user_id').notNull().references(()=> userTable.id, { onDelete: 'cascade' })
 });
 
-export const account = pgTable("account", {
+export const accountTable = pgTable("account", {
     id: text('id').primaryKey(),
     accountId: text('account_id').notNull(),
 providerId: text('provider_id').notNull(),
